e253ccedcb917c5049a2c0dda1e71d853ec64b74
Huimin Jiang <hjiang7@mail.sfsu.edu>
2020-03-16 19:37:23 -0700

    Initial Commit

---
+++ ExtraCredit-FoodPriceArray-Huimin-Jiang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExtraCredit-FoodPriceArray-Huimin-Jiang
+//
+//  Created by Huimin Jiang on 3/16/20.
+//  Copyright © 2020 Huimin Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
